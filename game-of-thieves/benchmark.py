5415b341c0db75f5b7389fe74bec24f1dba2a682
Armand Duijn <armand@duijn.space>
2018-12-27 21:48:26 +0100

    Add timer for base implementation
    A simple interface was added to be able to implement different versions of the algorithm without having to change the benchmark. Furthermore, a function was added to export the graph as a PDF to help understand the graph that is randomly generated.

---
+++ game-of-thieves/benchmark.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractmethod
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import timeit
+import GOT
+import library
+
+
+def visualize(G, filename):
+    """
+    Generates a visualization of the graph
+
+    :param G: NetworkX graph object
+    :param filename: Filename of the PDF
+    :return: void
+    """
+    plt.figure(figsize=(20, 20))
+
+    positions = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G=G, pos=positions, node_size=5)
+    nx.draw_networkx_edges(G=G, pos=positions, alpha=0.3)
+
+    plt.axis('off')
+    plt.savefig(filename + '.pdf')
+
+
+class AbstractAlgorithm(ABC):
+    def __init__(self, G):
+        self.network = G
+
+    @abstractmethod
+    def execute(self, **kwargs):
+        """
+        Executes the algorithm
+
+        :param kwargs: Arguments
+        :return:
+        """
+        pass
+
+    def time(self, number=5, **kwargs):
+        """
+        Times the running time of the algorithm
+
+        :param number: Number of times
+        :param kwargs: Arguments for the algorithm
+        :return: Average running time in seconds
+        """
+        seconds = timeit.timeit(stmt=lambda: self.execute(**kwargs), number=number)
+
+        return seconds/number
+
+
+class BaseImplementation(AbstractAlgorithm):
+    def execute(self, **kwargs):
+        return GOT.ComputeCentrality(G=self.network, **kwargs)
+
+
+G = library.generateNetwork(N=10, cnType='scale-free', weighted=True)
+
+# Export a visualization to understand what the GoT is calculating on
+visualize(G, 'graph')
+
+# Perform GoT algorithm
+settings = {
+    'noThiefs': 3,      # Number of thieves per node
+    'noVDiamonds': 10,  # Number of vdiamonds per node
+    'noEpochs': 1000,   # Number of epochs
+}
+
+iterations = 3  # Number of times to run the algorithm
+
+algorithm = BaseImplementation(G)
+seconds = algorithm.time(number=iterations, **settings)
+
+print('Average time ({} iterations): {:0.4f}s'.format(iterations, seconds))
